de33e018068375b4bc157ad92273978fa24b56fb
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-01-30 13:57:22 +0100

    fix: job offer add link

--- app/(public)/job-offers/page.tsx
+++ app/(public)/job-offers/page.tsx
@@ -4,6 +4,7 @@ import { Suspense } from "react";
 import { OffersCount } from "./components/OffersCount";
 import { OffersList } from "./components/OffersList";
 import { Metadata } from "next";
+import { PlusIcon } from "@heroicons/react/24/outline";
 
 export const metadata: Metadata = {
   title: "Job offers",
@@ -13,7 +14,11 @@ export default async function JobOffersPage() {
   return (
     <div>
       <Header>Job offers</Header>
-      <Link href="/job-offers/create">Create new offer</Link>
+      <Link href="/job-offers/create" className="block underline my-4">
+        <span className="flex">
+          <PlusIcon width={20} height={20} /> Create new offer
+        </span>
+      </Link>
 
       <Suspense fallback={<p>Loading count...</p>}>
         <OffersCount />
