d29669c5bcbb35e0ab70e8a62c90cb08731ab8fe
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-01-30 11:40:08 +0100

    feat: test with types

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,14 @@
+import { JobOffer } from "@prisma/client";
+
 export default function Page() {
+  const jobOffer: JobOffer = {
+    title: "Abc",
+    description: "lorem ipsum",
+    employer: "intel",
+    position: "Frontend developer",
+    salary: 10000,
+  };
+
   return (
     <main className="container p-6">
       <h1>Dashboard page</h1>
